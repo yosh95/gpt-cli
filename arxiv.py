13414c289614a6f07be7755163d15e264aa441e8
Yoshihiro Ando <yosh5295@gmail.com>
2024-03-19 22:34:18 +0900

    Use bs4 to scrape arxiv.org

--- arxiv.py
+++ arxiv.py
@@ -1,57 +1,72 @@
 import argparse
-import feedparser
 import os
+import requests
 
+from bs4 import BeautifulSoup
 
 def get_arxiv(category):
 
-    start=0
+    skip = 0
 
-    base_url="https://export.arxiv.org/api/query" \
-             + "?search_query=cat:{category}&start={start}" \
-             + "&sortBy=lastUpdatedDate&sortOrder=descending&max_results=10"
+    base_url = "https://arxiv.org/list/{category}/new?skip={skip}&show=10"
 
     while True:
-        d = feedparser.parse(base_url.format(category=category, start=start))
-        if len(d.entries) > 0:
-            items = []
-            for entry in d.entries:
-                id = entry.id.split("/")[-1]
-                title = entry.title.replace("\n", "")
-                items.append({"id":id,"title":title})
+        response = requests.get(base_url.format(category=category, skip=skip))
 
-            while True:
-                print("---")
-                for i, item in enumerate(items):
-                    print(f"{i}) {item['title']} ({item['id']})")
-                print("---")
-                user_input = input("(q to quit)> ")
+        if response.status_code == 200:
+            html_content = response.text
+        else:
+            print(f"Failed to retrieve the web page: {response.status_code}")
+            html_content = ""
+            exit()
 
-                if user_input == 'q':
-                    break
-                elif user_input == '':
-                    start += 10
-                    break
-                else:
-                    try:
-                        num = int(user_input)
-                    except ValueError:
-                        print(f"Invalid input:{user_input}")
-                        continue
+        soup = BeautifulSoup(html_content, "html.parser")
 
-                if num >= 0 and num <= 9:
-                    print(f"{num}) {items[num]['title']} ({items[num]['id']})")
-                    print("---")
-                    id = items[num]['id']
-                    command = f"gpt https://arxiv.org/pdf/{id}.pdf"
-                    print(command)
-                    os.system(command)
-                else:
-                    print(f"Invalid input:{user_input}")
+        arxiv_ids = []
+
+        for link in soup.find_all("a"):
+            href = link.get("href")
+            title = link.get("title")
+            if title == "Download PDF":
+                arxiv_id = href.split("/")[-1]
+                arxiv_ids.append(arxiv_id)
+
+        titles = []
+
+        for div in soup.find_all("div", class_="list-title mathjax"):
+            titles.append(div.text.strip().replace("Title: ", ""))
+
+        while True:
+            print("---")
+            print(f"(category:{category})")
+            for i, item in enumerate(arxiv_ids):
+                print(f"{i}) {titles[i]} ({arxiv_ids[i]})")
+            print("---")
+            user_input = input("(q to quit)> ")
+            print("---")
 
             if user_input == 'q':
                 break
-        else:
+            elif user_input == '':
+                skip += 10
+                break
+            else:
+                try:
+                    num = int(user_input)
+                except ValueError:
+                    print(f"Invalid input:{user_input}")
+                    continue
+
+            if num >= 0 and num <= 9:
+                print(f"{num}) {titles[num]} ({arxiv_ids[num]})")
+                print("---")
+                command = f"gpt https://arxiv.org/pdf/{arxiv_ids[num]}.pdf"
+                print(command)
+                os.system(command)
+            else:
+                print(f"Invalid input:{user_input}")
+
+        if user_input == 'q':
             break
 
 
